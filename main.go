8b9b98ea23ae31bae90e4ad08d283b9e51dd23d7
stieme <2443708373@qq.com>
2024-08-09 15:40:13 +0800

    注册功能实现

--- main.go
+++ main.go
@@ -17,13 +17,13 @@ func main() {
 	// 数据库连接,打开text数数据库
 	db, err := sql.Open("mysql", "root:as556564996@tcp(127.0.0.1:3306)/text")
 	if err != nil {
-		log.Fatal("Database connection failed: ", err) //没链接到就打印错误信息
+		log.Fatal("数据库链接出错: ", err) //没链接到就打印错误信息
 	}
 	defer db.Close() //延迟函数。断开数据库链接
 
 	err = db.Ping()
 	if err != nil {
-		log.Fatal("Database ping failed: ", err)
+		log.Fatal("数据库未仍在链接: ", err)
 	}
 	//检测数据库链接是否任然有效，否则打印错误信息
 
@@ -48,16 +48,16 @@ func main() {
 			//处理错误
 			if err != nil {
 				if errors.Is(err, sql.ErrNoRows) { //error.Is检测两错误是否相同
-					http.Error(w, "User not found.", http.StatusNotFound) //404，没找到
+					http.Error(w, "用户不存在.", http.StatusNotFound) //404，没找到
 				} else {
-					http.Error(w, "Database error.", http.StatusInternalServerError) //500，服务器错误
+					http.Error(w, "数据库出错.", http.StatusInternalServerError) //500，服务器错误
 				}
 				return
 			}
 
 			// 直接比较明文密码，数据库中是明文，错误就显示无效密码
 			if user.Password != password {
-				http.Error(w, "Invalid password.", http.StatusUnauthorized)
+				http.Error(w, "无效密码/密码有误.", http.StatusUnauthorized)
 				return
 			}
 
@@ -66,6 +66,45 @@ func main() {
 		} else {
 			// 如果不是 POST 请求，显示登录表单
 			http.ServeFile(w, r, "/static/login.html") // 修改为正确的路径
+		}
+	})
+
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			r.ParseForm()
+			username := r.FormValue("username")
+			password := r.FormValue("password")
+			confirm_password := r.FormValue("confirm_password")
+
+			// 确保两次输入的密码相同
+			if password != confirm_password {
+				http.Error(w, "密码错误", http.StatusBadRequest)
+				return
+			}
+
+			// 检查用户名是否已存在
+			var user User
+			err := db.QueryRow("SELECT username FROM users WHERE username = ?", username).Scan(&user.Username)
+			if err == nil {
+				// 用户名已存在
+				http.Error(w, "用户已经存在", http.StatusConflict)
+				return
+			}
+			if !errors.Is(err, sql.ErrNoRows) {
+				// 数据库查询出错
+				http.Error(w, "数据库出错", http.StatusInternalServerError)
+				return
+			}
+
+			// 注册新用户
+			_, err = db.Exec("INSERT INTO users (username, password) VALUES (?, ?)", username, password) //插入数据
+			if err != nil {
+				// 处理错误，例如返回错误信息给客户端
+				http.Error(w, "联网服务错误", http.StatusInternalServerError)
+				return
+			}
+
+			http.Redirect(w, r, "/static/index.html", http.StatusFound)
 		}
 	})
 
